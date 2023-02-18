b381a7044dbeb0cfea5b35fc60784787ac27841a
Stanislav Gurov <sgurov@systematica-consulting.ru>
2023-02-18 17:09:58 +0300

    Initial commit

---
+++ src/main/java/letscode/sarafan/SarafanApplication.java
@@ -0,0 +1,18 @@
+package letscode.sarafan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+public class SarafanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SarafanApplication.class, args);
+    }
+
+}
